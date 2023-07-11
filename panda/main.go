593d315021d1e0fe9979eb1006e5821b725d7a23
bitcqs <bitcqs@163.com>
2023-07-11 17:06:32 +0800

    update

--- panda/main.go
+++ panda/main.go
@@ -18,10 +18,11 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:      "panda",
-		Fullscreen: true,
+		Fullscreen: false,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+		Frameless:        false,
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
